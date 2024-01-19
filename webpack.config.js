4ad409158de70ee9510c0f85c8b1e76035385c48
AnatolyGavrilov <sharamradov@gmail.com>
2024-01-19 22:13:28 +0300

    added mode, dynamic hash name

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    clean: true,
   },
 };
